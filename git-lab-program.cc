7c12dbd19208c765cd7a15c68acf6908c78afddd
BrysonFPalmer <bp509821@ohio.edu>
2021-08-31 16:58:31 -0400

    Added Lab Program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *        Author: Bryson Palmer
+ *        Date: 9/31/2021
+ *        Description: Lab 2 Program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!" << endl;
+    return 0;
+}// main
